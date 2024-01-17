aa70af76578b9102fc2c8ff1c2e935fc9508756e
keilinsmith <keilin.s24@k12.xrds.org>
2024-01-17 15:38:28 -0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template("index.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/products')
+def products():
+    return render_template("products.html")
+
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/confirmation')
+def confirmation():
+    name = request.args.get('username')
+    email = request.args.get('email')
+    phone = request.args.get('phone')
+    props = {
+        "name": name,
+        "email": email,
+        "phone": phone
+    }
+    return render_template("confirmation.html", data=props)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
